62a06dfb56a4d0b8667add56202368bf963074df
a <>
2022-10-17 17:09:03 +0900

    v0.9(error)

--- Assets/Point.cs
+++ Assets/Point.cs
@@ -6,13 +6,35 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Point : MonoBehaviour
 {
+    private GameObject Target;
+    [SerializeField] private List<MeshRenderer> RendererList = new List<MeshRenderer>();
+
     [HideInInspector] public Transform Node;
+
+    [SerializeField] private LayerMask mask;
 
     private void Awake()
     {
         Rigidbody rigid = GetComponent<Rigidbody>();
         rigid.constraints = RigidbodyConstraints.FreezeRotation;
+
+        Target = Resources.Load("Prefab/Enemy") as GameObject;
     }
+
+    void FindRenderer(GameObject _Obj)
+    {
+        for (int i = 0; i < _Obj.transform.childCount; ++i)
+        {
+            if (_Obj.transform.childCount > 0)
+                FindRenderer(_Obj.transform.GetChild(i).gameObject);
+
+            MeshRenderer renderer = _Obj.transform.GetChild(i).GetComponent<MeshRenderer>();
+
+            if (renderer != null)
+                RendererList.Add(renderer);
+        }
+    }
+
 
     private void OnDrawGizmos()
     {
@@ -28,24 +50,56 @@ public class Point : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.transform.tag != "Ground")
-        {
-            transform.position = new Vector3(
-                Random.Range(-25.0f, 25.0f),
-                Random.Range(10.0f, 25.0f),
-                Random.Range(-25.0f, 25.0f));
 
-            transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
-        }
-        else
+        if(collision.gameObject.layer != mask)
         {
-            /*
-            GameObject Obj = Instantiate(Resources.Load("Prefab/BoxEnemy") as GameObject);
+            StartCoroutine(Create());
+            return;
+        }
+
+        transform.position = new Vector3(
+            Random.Range(-25.0f, 25.0f),
+            Random.Range(10.0f, 25.0f),
+            Random.Range(-25.0f, 25.0f));
+
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    IEnumerator Create()
+    {
+        while (true)
+        {
+            if (transform.childCount >= 1)
+                continue;
+
+            RendererList.Clear();
+
+            GameObject Obj = Instantiate(Target);
+
 
             Obj.transform.position = transform.position;
-            
+
             Obj.transform.parent = transform;
 
+            FindRenderer(Obj);
+
+            yield return new WaitForSeconds(3.0f);
+
+            foreach (MeshRenderer meshRenderer in RendererList)
+            {
+                meshRenderer.material.shader = Shader.Find("Transparent/VertexLit");
+
+                if (meshRenderer.material.HasProperty("_Color"))
+                {
+                    Color color = meshRenderer.material.GetColor("_Color");
+
+                    meshRenderer.material.SetColor("_Color", new Color(color.r, color.g, color.b, 0.0f));
+
+                    StartCoroutine(SetColor(meshRenderer, color));
+                }
+            }
+
+            /*
             MeshRenderer meshRenderer = Obj.GetComponent<MeshRenderer>();
 
             meshRenderer.material.shader = Shader.Find("Transparent/VertexLit");
@@ -59,22 +113,23 @@ public class Point : MonoBehaviour
                 StartCoroutine(SetColor(meshRenderer, color));
             }
              */
+
         }
     }
 
-    /*
+    
     IEnumerator SetColor(MeshRenderer meshRenderer, Color color)
     {
-
         float fTime = 0.0f;
+
         while (fTime <= 255.0f)
         {
             yield return null;
 
             fTime += Time.deltaTime;
 
-            meshRenderer.material.SetColor("_Color", new Color(color.r, color.g, color.b, 0.0f));
+            meshRenderer.material.SetColor("_Color", new Color(color.r, color.g, color.b, fTime));
         }
     }
-     */
+    
 }
